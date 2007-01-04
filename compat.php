4588ef7c47bb540fe5e1add06fecfeb585467c9b
Steve Sanbeg <sanbeg@users.mediawiki.org>
2007-01-04 17:34:19 +0000

    Add comment block to describe compat.php

--- compat.php
+++ compat.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+
+This file contains compatability functions for other extensions which may
+have called them, primarily DynamicPageList2.  This file is not used by the
+Labeled Section Transclusion extension, but is provided to insulate other
+extensions from code refactoring.
+
+**/
+
 ///Fetch the page to be transcluded from the database.
 function wfLst_fetch_($parser, $page, $ns = NS_MAIN) 
 {
