3f3ab4e3d511b745f6616bc2caf4e6fb5b10bf49
Steve Sanbeg <sanbeg@users.mediawiki.org>
2007-11-20 22:50:43 +0000

    remove hack & restore some flags to old defaults.  We get 1 less regression this way; the rest are probably because the parser now ignores the headingOffset flag

--- lst.php
+++ lst.php
@@ -119,14 +119,14 @@ function wfLst_parse_($parser, $title, $text, $part1, $skiphead=0)
   if (wfLst_open_($parser, $part1)) {
     //Handle recursion here, so we can break cycles.
     global $wgVersion;
-    if( (version_compare( $wgVersion, "1.9" ) < 0) or method_exists('Parser','splitWhitespace') ) {
+    if( version_compare( $wgVersion, "1.9" ) < 0 ) {
       $text = $parser->replaceVariables($text);
       wfLst_close_($parser, $part1);
     }
     
     //Try to get edit sections correct by munging around the parser's guts.
     return array($text, 'title'=>$title, 'replaceHeadings'=>true, 
-		 'headingOffset'=>$skiphead);
+		 'headingOffset'=>$skiphead, 'noparse'=>false, 'noargs'=>false);
   }  else {
     return "[[" . $title->getPrefixedText() . "]]". 
       "<!-- WARNING: LST loop detected -->";
