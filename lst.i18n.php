6b228af77ee9fa21ad40aa62f72b55195d30fd79
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-20 15:37:10 +0000

    Localisation updates for extension messages from Betawiki (2008-02-10 16:10 CET)

--- lst.i18n.php
+++ lst.i18n.php
@@ -11,9 +11,80 @@ $messages['en'] = array(
 	'lst-desc' => 'Adds <tt>#lst</tt> and <tt>#lstx</tt> functions and <tt>&lt;section&gt;</tt> tag, enables marked sections of text to be transcluded',
 );
 
+/** Arabic (العربية)
+ * @author Meno25
+ */
+$messages['ar'] = array(
+	'lst-desc' => 'يضيف الدالتين <tt>#lst</tt> و <tt>#lstx</tt> والوسم <tt>&lt;section&gt;</tt>، يفعل تضمين الأقسام المعلمة من النص',
+);
+
+/** Czech (Česky)
+ * @author Matěj Grabovský
+ */
+$messages['cs'] = array(
+	'lst-desc' => 'Přidává funkce <tt>{{#lst:}}</tt> a <tt>{{#lstx:}}</tt> a tag <tt>&lt;section&gt;</tt>, umožňuje transkluzi označených částí textu',
+);
+
 /** German (Deutsch)
  * @author Raimond Spekking
  */
 $messages['de'] = array(
 	'lst-desc' => 'Fügt die <tt>#lst</tt> und <tt>#lstx</tt> Funktionen sowie das <tt>&lt;section&gt;</tt>-Tag hinzu, um so markierte Abschnitte eingebunden werden zu können',
 );
+
+/** French (Français)
+ * @author Grondin
+ */
+$messages['fr'] = array(
+	'lst-desc' => 'Ajoute les fonctions <tt>#lst</tt> et <tt>#lstx</tt> ainsi que la balise <tt>&lt;section&gt;</tt>, active les sections marquées du texte devant être transclus',
+);
+
+/** Japanese (日本語)
+ * @author JtFuruhata
+ */
+$messages['ja'] = array(
+	'lst-desc' => '文書を章単位で埋め込み参照可能とする <tt>#lst</tt>、<tt>#lstx</tt> 関数および <tt>&lt;section&gt;</tt> タグを追加する',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'lst-desc' => 'Voegt de functies <tt>#lst</tt> en <tt>#lstx</tt> en de tag <tt>&lt;section&gt;</tt> toe  voor het markeren van tekstdelen die getranscludeerd moeten worden',
+);
+
+/** Portuguese (Português)
+ * @author Malafaya
+ */
+$messages['pt'] = array(
+	'lst-desc' => 'Adiciona as funções <tt>#lst</tt> e <tt>#lstx</tt>, e a marca &lt;section&gt;, activa secções marcadas de texto para serem transcluídas',
+);
+
+/** Russian (Русский)
+ * @author .:Ajvol:.
+ */
+$messages['ru'] = array(
+	'lst-desc' => 'Добавляет функции <tt>#lst</tt> и <tt>#lstx</tt> и тег <tt>&lt;section&gt;</tt>, позволяет включать отмеченные участки текста на другие страницы',
+);
+
+/** Slovak (Slovenčina)
+ * @author Helix84
+ */
+$messages['sk'] = array(
+	'lst-desc' => 'Pridáva funkcie <tt>#lst</tt> and <tt>#lstx</tt> a značku <tt>&lt;section&gt;</tt>, umožňuje transklúziu označených častí textu',
+);
+
+/** Swedish (Svenska)
+ * @author Lejonel
+ */
+$messages['sv'] = array(
+	'lst-desc' => 'Lägger till funktionerna <tt>#lst</tt> och <tt>#lstx</tt> för att inkludera avsnitt av en text, samt taggen <tt>&lt;section&gt;</tt> för att markera de avsnitt som kan inkluderas',
+);
+
+/** Vietnamese (Tiếng Việt)
+ * @author Vinhtantran
+ */
+$messages['vi'] = array(
+	'lst-desc' => 'Thêm hàm <tt>#lst</tt> và <tt>#lstx</tt> và thẻ <tt>&lt;section&gt;</tt> để cho phép những đoạn văn bản được đánh dấu sẽ được nhúng.',
+);
+
