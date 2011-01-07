95dfd4fcd3911bcb2b5558b8d4d1305191398aad
Sam Reed <reedy@users.mediawiki.org>
2011-01-07 06:33:10 +0000

    Add missing return true; that was upsetting tesla! Will look for someone to blame in the morning

--- lst.php
+++ lst.php
@@ -49,6 +49,8 @@ class LabeledSectionTransclusion {
 				$parser->setFunctionHook( 'lst', array( __CLASS__, 'pfuncInclude' ) );
 				$parser->setFunctionHook( 'lstx', array( __CLASS__, 'pfuncExclude' ) );
 		}
+
+		return true;
 	}
 
 	// / Add the magic words - possibly with more readable aliases
