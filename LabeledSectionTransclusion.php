60fa3603392ca69c1584cca879f7deab408db8bb
Reedy <reedy@wikimedia.org>
2015-01-09 15:02:16 +0000

    Change canonical entry points around
    Change-Id: I1022a004c8cfdbf16f1afacfcb9b67be90c31e06

--- LabeledSectionTransclusion.php
+++ LabeledSectionTransclusion.php
@@ -1,10 +1,43 @@
 <?php
-# This is a conventional entry point for continuous integration.
-# Jenkins job expect a PHP file having the same name as the git
-# repository hosting the extension. Aka:
-#
-if( !defined('MEDIAWIKI') ) {
+if ( ! defined( 'MEDIAWIKI' ) ) {
 	die();
 }
 
-require_once( 'lst.php'  );
+/**#@+
+ * A parser extension that adds two functions, #lst and #lstx, and the
+ * <section> tag, for transcluding marked sections of text.
+ *
+ * @file
+ * @ingroup Extensions
+ *
+ * @link http://www.mediawiki.org/wiki/Extension:Labeled_Section_Transclusion Documentation
+ *
+ * @bug 5881
+ *
+ * @author Steve Sanbeg
+ * @copyright Copyright © 2006, Steve Sanbeg
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+$wgHooks['ParserFirstCallInit'][] = 'LabeledSectionTransclusion::setup';
+// @todo FIXME: LanguageGetMagic is obsolete, but LabeledSectionTransclusion::setupMagic()
+//              contains magic hack that $magicWords cannot handle.
+$wgHooks['LanguageGetMagic'][] = 'LabeledSectionTransclusion::setupMagic';
+
+$wgExtensionCredits['parserhook'][] = array(
+	'path'           => __FILE__,
+	'name'           => 'LabeledSectionTransclusion',
+	'author'         => 'Steve Sanbeg',
+	'url'            => 'https://www.mediawiki.org/wiki/Extension:Labeled_Section_Transclusion',
+	'descriptionmsg' => 'lst-desc',
+);
+
+$dir = __DIR__;
+$wgAutoloadClasses['LabeledSectionTransclusion'] = $dir . '/LabeledSectionTransclusion.class.php';
+$wgParserTestFiles[] = $dir . '/lstParserTests.txt';
+$wgParserTestFiles[] = $dir . '/lstIncorrectParserTest.txt';
+$wgParserTestFiles[] = $dir . '/lsthParserTests.txt';
+$wgMessagesDirs['LabeledSectionTransclusion'] = __DIR__ . '/i18n';
+$wgExtensionMessagesFiles['LabeledSectionTransclusion'] = $dir . '/lst.i18n.php';
+
+$wgLstLocal = null;
--- lst.php
+++ lst.php
@@ -1,43 +1,7 @@
 <?php
-if ( ! defined( 'MEDIAWIKI' ) ) {
+# Holdover entry point
+if( !defined('MEDIAWIKI') ) {
 	die();
 }
 
-/**#@+
- * A parser extension that adds two functions, #lst and #lstx, and the
- * <section> tag, for transcluding marked sections of text.
- *
- * @file
- * @ingroup Extensions
- *
- * @link http://www.mediawiki.org/wiki/Extension:Labeled_Section_Transclusion Documentation
- *
- * @bug 5881
- *
- * @author Steve Sanbeg
- * @copyright Copyright © 2006, Steve Sanbeg
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
- */
-
-$wgHooks['ParserFirstCallInit'][] = 'LabeledSectionTransclusion::setup';
-// @todo FIXME: LanguageGetMagic is obsolete, but LabeledSectionTransclusion::setupMagic()
-//              contains magic hack that $magicWords cannot handle.
-$wgHooks['LanguageGetMagic'][] = 'LabeledSectionTransclusion::setupMagic';
-
-$wgExtensionCredits['parserhook'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'LabeledSectionTransclusion',
-	'author'         => 'Steve Sanbeg',
-	'url'            => 'https://www.mediawiki.org/wiki/Extension:Labeled_Section_Transclusion',
-	'descriptionmsg' => 'lst-desc',
-);
-
-$dir = __DIR__;
-$wgAutoloadClasses['LabeledSectionTransclusion'] = $dir . '/LabeledSectionTransclusion.class.php';
-$wgParserTestFiles[] = $dir . '/lstParserTests.txt';
-$wgParserTestFiles[] = $dir . '/lstIncorrectParserTest.txt';
-$wgParserTestFiles[] = $dir . '/lsthParserTests.txt';
-$wgMessagesDirs['LabeledSectionTransclusion'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['LabeledSectionTransclusion'] = $dir . '/lst.i18n.php';
-
-$wgLstLocal = null;
+require_once( 'LabeledSectionTransclusion.php'  );
