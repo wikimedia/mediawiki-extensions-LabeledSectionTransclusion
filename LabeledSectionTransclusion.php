e42d3ca92830c15f9772f86faf4dffdaf2f74457
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 20:49:34 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: I1e39c1baa40b7d085649a8b44cef383af35dba35

--- LabeledSectionTransclusion.php
+++ LabeledSectionTransclusion.php
@@ -20,11 +20,11 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'LabeledSectionTransclusion' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['LabeledSectionTransclusion'] = __DIR__ . '/i18n';
-	/*wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for LabeledSectionTransclusion extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	);*/
+	);
 	return;
 } else {
 	die( 'This version of the LabeledSectionTransclusion extension requires MediaWiki 1.25+' );
